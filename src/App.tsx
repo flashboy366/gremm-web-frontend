555b94826c6d9b800bb63c938bcb699c085abdbe
Kirill Platonov <platonovk1998@ya.ru>
2023-12-12 22:17:56 +0400

    setup fonts and colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
 function App() {
-    return <div className="text-3xl text-red-700">yes</div>;
+    return (
+        <div className="flex flex-col gap-7">
+            <div className="font-corner-title">Corner Title</div>
+            <div className="font-link-title">link title</div>
+            <div className="font-h1">H1</div>
+            <div className="font-h2">h2</div>
+            <div className="font-h3">h3</div>
+            <div className="font-h4">h4</div>
+            <div className="font-button-1">button 1</div>
+            <div className="font-button-2">Button 2</div>
+            <div className="font-text-1">Text 1</div>
+            <div className="font-text-2">Text 2</div>
+            <div className="font-discount-text">discount text</div>
+            <div className="font-news-headline">News Headline</div>
+            <div className="font-news-text">News Text</div>
+            <div className="font-numeric-1">Numeric 1</div>
+            <div className="font-numeric-2">numeric 2</div>
+            <div className="font-caption">Caption</div>
+        </div>
+    );
 }
 
 export default App;
